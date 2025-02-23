ca9c80f019faebba86fe0241825255474979128c
Joel <maijol60@gmail.com>
2025-02-23 23:41:36 +0100

    logo en fondo

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createGlobalStyle } from 'styled-components';
 import WinToolbar from './componentes/WinToolbar.jsx'
-
-const GlobalStyles = createGlobalStyle`
-  body {
-    background-color: teal;
-    margin: 0;
-    padding: 0;
-    font-family: 'ms_sans_serif';
-  }
-`;
+import './assets/css/fondo.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyles />
     <WinToolbar />
   </StrictMode>
 );
