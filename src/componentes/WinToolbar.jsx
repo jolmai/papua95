2099da50a9d70484252547be33c439f54597b92f
Joel <maijol60@gmail.com>
2025-02-24 09:46:24 +0100

    aparece lista al pulsar boton incio e iconos

--- src/componentes/WinToolbar.jsx
+++ src/componentes/WinToolbar.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Button, AppBar, Toolbar } from 'react95';
+import React, {useState} from 'react';
+import { Button, AppBar, Toolbar, MenuList, MenuListItem, Separator} from 'react95';
 import { ThemeProvider } from 'styled-components';
 import original from 'react95/dist/themes/original';
 import logo from '../assets/img/logo.png';
+import libro from '../assets/img/iconos/book.ico';
+import configuracion from '../assets/img/iconos/Settings.ico';
+import programas from '../assets/img/iconos/programs.ico';
+import apagar from '../assets/img/iconos/off.ico';
+import documentos from '../assets/img/iconos/web-documents.ico';
+import find from '../assets/img/iconos/find.ico';
+import ejecutar from '../assets/img/iconos/run.ico';
 import '../assets/css/toolbar.css';
 
+function MenuItem({icono, alter, texto}) {
+  return (
+    <MenuListItem className='itemMenu'>
+      <div className='itemFlex'>
+        <img src={icono} alt={alter} style={{height: '30px', marginRight: '10px'}}/>
+        {texto}
+      </div>
+    </MenuListItem>
+  )
+}
 
 function WinToolbar() {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <ThemeProvider theme={original}>
       <AppBar className='toolbarposition'>
         <Toolbar>
           <div>
-            <Button style={{ fontWeight: 'bold' }}>
-              <img src={logo} alt="logo papua" style={{height: '30px', marginRight: '10px'}}/>
-              Start</Button>
+            <Button
+              onClick={() => setOpen(!open)}
+              active={open}
+            >
+            <img src={logo} alt="logo papua" style={{height: '30px', marginRight: '10px'}}/>
+            Inicio</Button>
+            {open && (
+              <MenuList className='listaMenu' onClick={() => setOpen(false)}>
+                <MenuItem icono={programas} alt={'icono-programas'} texto='Programas' />
+                <MenuItem icono={documentos} alt={'icono-documentos'} texto='Documentos' />
+                <MenuItem icono={configuracion} alt={'icono-configuracion'} texto='Configuración' />
+                <MenuItem icono={find} alt={'icono-buscar'} texto='Buscar' />
+                <MenuItem icono={libro} alt={'icono-ayuda'} texto='Ayuda' />
+                <MenuItem icono={ejecutar} alt={'icono-ejecutar'} texto='Ejecutar...' />
+                <Separator />
+                <MenuItem icono={apagar} alt={'icono-apagar'} texto='Apagar' />
+              </MenuList>
+            )}
           </div>
         </Toolbar>
         
