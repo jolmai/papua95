2d15f0c4de454a6afb4d66ae3c0e2b2b5c120a8a
uripinyi <uripinyi@gmail.com>
2025-02-25 10:16:00 +0100

    Al pulsar afuera se cierra menu

--- src/componentes/WinToolbar.jsx
+++ src/componentes/WinToolbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { Button, AppBar, Toolbar, MenuList, MenuListItem, Separator, Tooltip} from 'react95';
 import { ThemeProvider } from 'styled-components';
 import original from 'react95/dist/themes/original';
@@ -12,6 +12,7 @@ import find from '../assets/img/iconos/find.ico';
 import ejecutar from '../assets/img/iconos/run.ico';
 import '../assets/css/toolbar.css';
 import TiempoActual from './TiempoActual.jsx'
+import PropTypes from 'prop-types';
 
 function MenuItem({icono, alter, texto}) {
   return (
@@ -24,20 +25,39 @@ function MenuItem({icono, alter, texto}) {
   )
 }
 
-function WinToolbar() {
+MenuItem.propTypes = {
+  icono: PropTypes.string.isRequired,
+  alter: PropTypes.string.isRequired,
+  texto: PropTypes.string.isRequired
+};
 
+function WinToolbar() {
   const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   return (
     <ThemeProvider theme={original}>
       <AppBar className='toolbarposition'>
         <Toolbar className='toolbarFlex'>
+          <div ref={menuRef}>
             <Button
               onClick={() => setOpen(!open)}
               active={open}
             >
-            <img src={logo} alt="logo papua" style={{height: '25px', marginRight: '10px'}}/>
-            Inicio</Button>
+              <img src={logo} alt="logo papua" style={{height: '25px', marginRight: '10px'}}/>
+              Inicio
+            </Button>
             {open && (
               <MenuList className='listaMenu' onClick={() => setOpen(false)}>
                 <MenuItem icono={programas} alt={'icono-programas'} texto='Programas' />
@@ -50,9 +70,10 @@ function WinToolbar() {
                 <MenuItem icono={apagar} alt={'icono-apagar'} texto='Apagar' />
               </MenuList>
             )}
-            <Tooltip text= 'Hora local (CET)' enterDelay={100} leaveDelay={100}>
-              <TiempoActual />
-            </Tooltip> 
+          </div>
+          <Tooltip text= 'Hora local (CET)' enterDelay={100} leaveDelay={100}>
+            <TiempoActual />
+          </Tooltip> 
         </Toolbar>
       </AppBar>
     </ThemeProvider>
