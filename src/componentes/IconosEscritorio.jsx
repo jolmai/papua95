f3e990e5c9a452c4def8aac4bc49b2a6468c9d18
J <maijol60@gmail.com>
2025-02-26 09:40:21 +0100

    se abren ventanas iconos escritorio

--- src/componentes/IconosEscritorio.jsx
+++ src/componentes/IconosEscritorio.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import { Frame, Tooltip } from 'react95';
+import { Frame, Tooltip, Window, WindowContent, WindowHeader, Button } from 'react95';
 import MiPC from '../assets/img/iconos/mipc.ico';
 import Papelera from '../assets/img/iconos/papelera.ico';
 import Carpeta from '../assets/img/iconos/carpeta.ico';
 import '../assets/css/icon.css';
 
-function Iconos ({icono, alter, nombre, idIcono}) {
+function Iconos ({icono, alter, nombre, idIcono, onClick}) {
     return(
         <Tooltip style={{backgroundColor: 'white'}} text={nombre} enterDelay={100} leaveDelay={100} position='bottom'>
-        <Frame className='icono' onClick={() => eligeVentanaAbierta({idIcono})}>
+        <Frame className='icono' onClick={() => onClick(idIcono)}>
             <img src={icono} alt={alter} style={{width: '50px'}}/>
-            <p>{nombre}</p>
+            <p className='nombreIcono'>{nombre}</p>
         </Frame>
         </Tooltip>
     );
@@ -29,11 +29,37 @@ function IconosEscritorio() {
     };
 
     return(
-        <div className='containerIconos'>
-            <Iconos icono={MiPC} alter={'icono-mipc'} nombre={'Mi Pc'} idIcono={'mipc'} />
-            <Iconos icono={Papelera} alter={'icono-papelera'} nombre={'Papelera'} idIcono={'papelera'} />
-            <Iconos icono={Carpeta} alter={'icono-carpeta'} nombre={'La funda'} idIcono={'funda'} />
-        </div>
+        <>
+            <div className='containerIconos'>
+                <Iconos icono={MiPC} alter={'icono-mipc'} nombre={'Mi Pc'} idIcono={'mipc'} onClick={cogerClickIcono}/>
+                <Iconos icono={Papelera} alter={'icono-papelera'} nombre={'Papelera'} idIcono={'papelera'} onClick={cogerClickIcono}/>
+                <Iconos icono={Carpeta} alter={'icono-carpeta'} nombre={'La funda'} idIcono={'funda'} onClick={cogerClickIcono}/>
+            </div>
+
+            {ventanaAbierta === 'mipc' && (
+                <Window resizable className='ventanaCompleta'> 
+                    <WindowHeader className='barraVentana'>
+                        <p>Mi PC</p>
+                        <Button onClick={cerrarVentana} style={{ marginLeft: 'auto' }}>X</Button>
+                    </WindowHeader>
+                    <WindowContent style={{backgroundColor: 'white'}}>
+                        <p>Papua manda</p>
+                    </WindowContent>
+                    
+                </Window>
+            )}
+            {ventanaAbierta === 'papelera' && (
+                <Window resizable className='ventanaCompleta'> 
+                    <WindowHeader className='barraVentana'>
+                        <p>Papelera</p>
+                        <Button onClick={cerrarVentana}>X</Button>
+                    </WindowHeader>
+                    <WindowContent style={{backgroundColor: 'white'}}>
+                        <p>papelera</p>
+                    </WindowContent>    
+                </Window>
+            )}
+        </>
     );
 }
 
