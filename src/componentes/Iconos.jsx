05783ea19bdfc2cc5eed0ec364c9ce181ea32492
jolmai <74318060+jolmai@users.noreply.github.com>
2025-03-04 17:56:15 +0000

    las ventanas son mas grandes, se abren en sitios diferentes y se cierra una a una

--- src/componentes/Iconos.jsx
+++ src/componentes/Iconos.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Frame, Tooltip } from 'react95';
 
-function Iconos ({icono, alter, nombre, idIcono, onClick}) {
+function Iconos ({icono, alter, nombre, idIcono, onClick, estaDentroCarpeta}) {
     return(
         <Tooltip style={{backgroundColor: 'white'}} text={nombre} enterDelay={100} leaveDelay={100} position='bottom'>
         <Frame variant="well" className='icono' onClick={() => onClick(idIcono)}>
             <img src={icono} alt={alter} style={{width: '50px'}}/>
-            <p className='nombreIcono'>{nombre}</p>
+            <p className='nombreIcono' style={{ color: estaDentroCarpeta ? 'black' : 'white' }}>{nombre}</p>
         </Frame>
         </Tooltip>
     );
--- src/componentes/VentanaMiPC.jsx
+++ src/componentes/VentanaMiPC.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Ventana from "./Ventana";
 
-function VentanaMiPC({onClose}){
+function VentanaMiPC({onClose, posicion}){
     return(
-        <Ventana titulo='MiPC' onClose={onClose}>
+        <Ventana titulo='MiPC' onClose={onClose}
+        style={{ 
+            position: 'absolute',
+            left: `${posicion.x}px`, // Posición X
+            top: `${posicion.y}px`, // Posición Y
+        }}
+        >
             <p>Mipc bebe</p>
         </Ventana>
     );
