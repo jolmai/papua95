74d78af6581d4b1bd1302e0f9e5617b7580b78b4
jolmai <74318060+jolmai@users.noreply.github.com>
2025-03-10 16:51:58 +0000

    arreglado que no se abran los juegos en la funda

--- src/componentes/Iconos.jsx
+++ src/componentes/Iconos.jsx
@@ -3,12 +3,19 @@ import { Frame, Tooltip } from 'react95';
 
 function Iconos ({icono, alter, nombre, idIcono, onClick, estaDentroCarpeta}) {
     return(
+        <div 
+            onClick={(e) => {
+                e.stopPropagation();
+                onClick();
+            }}
+        >
         <Tooltip style={{backgroundColor: 'white'}} text={nombre} enterDelay={100} leaveDelay={100} position='bottom'>
         <Frame variant="well" className='icono' onClick={() => onClick(idIcono)}>
             <img src={icono} alt={alter} style={{width: '50px'}}/>
             <p className='nombreIcono' style={{ color: estaDentroCarpeta ? 'black' : 'white' }}>{nombre}</p>
         </Frame>
         </Tooltip>
+        </div>
     );
 }
 
--- src/componentes/Juegos.js
+++ src/componentes/Juegos.js
@@ -1,9 +1,9 @@
 const juegos = [
-    { id: 'dodge', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
-    { id: 'dodge', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
-    { id: 'dodge', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
-    { id: 'dodge', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
-    { id: 'dodge', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'}
+    { id: 'dodge1', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
+    { id: 'dodge2', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
+    { id: 'dodge3', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
+    { id: 'dodge4', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
+    { id: 'dodge5', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'}
 ];
 
 export default juegos;
