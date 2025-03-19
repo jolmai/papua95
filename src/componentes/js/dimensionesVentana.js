785611116481b1b512a871ff47bc855a6e36eb89
Joel <maijol60@gmail.com>
2025-03-19 09:40:47 +0100

    no tiene borde la seleccion.

--- src/componentes/js/dimensionesVentana.js
+++ src/componentes/js/dimensionesVentana.js
@@ -1,7 +1,7 @@
 const dimensionVentana = {
     mipc: {width: 450, height: 350},
     papelera: {width: 450, height: 350},
-    funda: {width: 430, height: 400}
+    funda: {width: 450, height: 400}
 }
 
 export default dimensionVentana;
