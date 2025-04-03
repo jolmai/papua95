ece00409ab3f517c00a665f4fef93acc655ef45f
Joel <maijol60@gmail.com>
2025-04-03 10:46:02 +0200

    añadido juego e icono

--- src/componentes/js/dimensionesVentana.js
+++ src/componentes/js/dimensionesVentana.js
@@ -1,10 +1,11 @@
 const dimensionVentana = {
     mipc: {width: 450, height: 350},
     papelera: {width: 450, height: 350},
-    funda: {width: 425, height: 400},
+    funda: {width: 513, height: 400},
     rana:{width: 900, height: 600},
     dungeon:{width: 800, height: 560},
     peixe:{width: 1200, height: 800},
+    nostalgia:{width: 1200, height: 800}
 }
 
 export default dimensionVentana;
