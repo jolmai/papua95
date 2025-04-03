14bcaea98970e0751447408c2318bb16b2e5610b
Joel <maijol60@gmail.com>
2025-04-03 10:46:02 +0200

    añadido juego e icono

--- src/componentes/js/Juegos.js
+++ src/componentes/js/Juegos.js
@@ -1,8 +1,8 @@
 const juegos = [
     { id: 'rana', icono: '/icono-juego/LaRana_Icon.jpg', nombre: 'Sapu Esquiva', iframeSrc: '/juegos/ranix+/ranix.html'},
     { id: 'dungeon', icono: '/icono-juego/dungeon_icon.jpg', nombre: 'Masmorra', iframeSrc: '/juegos/dungeon+/Dungeon.html'},
+    { id: 'nostalgia', icono: '/icono-juego/nostalgiadutemp_icon.jpg', nombre: 'Nostalgia du Tempoo', iframeSrc: '/juegos/nostalgia+/Nostalgia.html'},
     { id: 'peixe', icono: '/icono-juego/pelepeixe.jpg', nombre: 'Pelé Peixe', iframeSrc: '/juegos/Pelé Peixe/pescaos.html'},
-    { id: 'dodge4', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
     { id: 'dodge5', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'}
 ];
 
