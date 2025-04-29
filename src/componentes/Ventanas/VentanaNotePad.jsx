c9349cd755f9f2e7b13b2ac5126a1550efb3d254
nick <you@example.com>
2025-04-29 09:54:05 +0200

    black cock

--- src/componentes/Ventanas/VentanaNotePad.jsx
+++ src/componentes/Ventanas/VentanaNotePad.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Icono from "../Utiles/Iconos";
 import Ventana from "../Utiles/Ventana";
 import BlocDeNotas from '../../assets/img/iconos/Notepad.ico';
+import { ScrollView, TextInput } from 'react95';
 
 function VentanaBlocDeNotas({ onClose, posicion, isFocused, onClick }) {
+    const [text, setText] = useState('');
+    const [contentHeight, setContentHeight] = useState(400); // Default height
+    const textAreaRef = useRef(null);
+
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleOpen = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setText(e.target.result);
+            };
+            reader.readAsText(file);
+        }
+    };
+
     return ( 
         <Ventana titulo='Bloc de notas' onClose={onClose} tipoVentana="BlocDeNotas" iconHeader={BlocDeNotas}
         style={{ 
