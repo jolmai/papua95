15b72faf493b9889ddba5618ee7fb4677f3aab47
Joel <maijol60@gmail.com>
2025-05-07 15:16:36 +0200

    jogos

--- src/componentes/js/Juegos.js
+++ src/componentes/js/Juegos.js
@@ -1,7 +1,7 @@
 const juegos = [
-    { id: 'rana', icono: '/icono-juego/LaRana_Icon.jpg', nombre: 'Sapu Esquiva', iframeSrc: '/juegos/Ranix+/ranix.html'},
-    { id: 'dungeon', icono: '/icono-juego/dungeon_icon.jpg', nombre: 'Masmorra', iframeSrc: '/juegos/Dungeon+/Dungeon.html'},
-    { id: 'nostalgia', icono: '/icono-juego/nostalgiadutemp_icon.jpg', nombre: 'Nostalgia du Tempoo', iframeSrc: '/juegos/Nostalgia+/Nostalgia.html'},
+    { id: 'rana', icono: '/icono-juego/LaRana_Icon.jpg', nombre: 'Sapu Esquiva', iframeSrc: '/juegos/ranix+/ranix.html'},
+    { id: 'dungeon', icono: '/icono-juego/dungeon_icon.jpg', nombre: 'Masmorra', iframeSrc: '/juegos/dungeon+/Dungeon.html'},
+    { id: 'nostalgia', icono: '/icono-juego/nostalgiadutemp_icon.jpg', nombre: 'Nostalgia du Tempoo', iframeSrc: '/juegos/nostalgia+/Nostalgia.html'},
     { id: 'peixe', icono: '/icono-juego/pelepeixe.jpg', nombre: 'Pelé Peixe', iframeSrc: '/juegos/Pelé Peixe/pescaos.html'},
     { id: 'dodge5', icono: '/icono-juego/dodge.png', nombre: 'Dodge', iframeSrc: '/juegos/dodge/index.html'},
     { id: 'dino', icono: '/icono-juego/icono-cachondino.png', nombre: 'Cachondino 2 Demo', iframeSrc: '/juegos/cachondin2+/Cachondintu.html'}
